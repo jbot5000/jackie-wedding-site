e10534c6b2e99a0e7cbf3521e3e5378fb308376e
benson.jesse.j <benson.jesse.j@gmail.com>
2020-11-14 14:40:44 -0500

    updated for post marriage

--- src/pages/index.js
+++ src/pages/index.js
@@ -16,33 +16,21 @@ const donateLink = `https://secure.actblue.com/donate/naacp-1`;
 const IndexPage = () => (
   <Layout>
     <SEO title="Jackie & Jon's Wedding" />
-    {
-      Countdown ? <h2>We're getting married!</h2> : <h2>We're Married!</h2>
-    }
+    <h2>We got Married!</h2>
     <p><span className="date">1:00pm Saturday, November 14th, 2020, in Austin, TX.</span>{Countdown ? <Countdown></Countdown> : ''}</p>
     <section id="links">
-      {
-        Countdown ?
-        <div className="link">
-          <a href={youtubeLink}>
-            <button><img src={yt} alt="YouTube logo" /><span>Live Stream</span></button>
-          </a>
-          <p>Stay safe from covid-19 and join us virtually via YouTube Live! Join at 12:45pm to see the ceremony at 1pm, or check back later for a recording.</p>
-        </div>
-        :
         <div className="link">
           <a href={youtubeLink}>
             <button><img src={yt} alt="YouTube logo" /><span>Recording</span></button>
           </a>
           <p>Here's the recording of our ceremony! We did it! <span role="img" aria-label="tada">🎉</span></p>
         </div>
-      }
-      <div className="link">
-        <a href={paypalLink}>
-          <button><img src={pp} alt="PayPal logo" /><span>Wedding Fund</span></button>
-        </a>
-        <p>If you'd like to pitch in, we're accepting donations to help cover the cost of our small wedding and growing a nest egg.</p>
-      </div>
+        <div className="link">
+            <a href={paypalLink}>
+              <button><img src={pp} alt="PayPal logo" /><span>Wedding Fund</span></button>
+            </a>
+            <p>If you'd like to pitch in, we're accepting donations to help cover the cost of our small wedding and growing a nest egg.</p>
+        </div>
       <div className="link">
         <a href={donateLink} target="_blank" rel="noreferrer">
           <button><img src={donate} alt="Donate to NAACP" /><span>Donate to NAACP</span></button>
@@ -51,7 +39,7 @@ const IndexPage = () => (
       </div>
     </section>
     <section>
-      <span className="fyi">Thank you!</span> We appreciate you visiting our site. Feel special we invited you! <span role="img" aria-label="sparkle">✨</span>
+      <span className="fyi">Thank you!</span> We appreciate you visiting our wedding site! <span role="img" aria-label="sparkle">✨</span>
     </section>
   </Layout>
 )
